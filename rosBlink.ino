2f6380086e4fc7035ed8be0993fa7aa6c0f7170f
Dillon MacEwan <dillon@mekazoic.com>
2017-03-27 22:59:37 +1100

    Update rosBlink.ino

--- rosBlink.ino
+++ rosBlink.ino
@@ -1,2 +1,19 @@
 // Rosserial Arduino subscriber example
 
+#include <ros.h>
+#include <std_msg/Empty.h>
+
+//create a ROS Nodehandle object
+
+ros::NodeHandle nh;
+
+// create a callback for the topic toggle_led,
+//whenever a value comes through this topic
+//this callback will execute
+//The callback will toggle the led on pin 13
+
+void messageCb(const std_msgs::Empty&toggle_msg)
+{
+  digitalWrite(13, HIGH-digitalRead13); //blink the LED
+}
+
